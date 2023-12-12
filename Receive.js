5eefa7f1b0eea30cf0a67c5857a9414766a6aec2
Isaac <68208158+Imacat-PlaysGames@users.noreply.github.com>
2023-12-12 11:57:29 -0500

    Update Receive.js

--- Receive.js
+++ Receive.js
@@ -2,17 +2,17 @@ enum RadioMessage {
     message1 = 49434,
     OllieOzMarthaChachiPoppyBobbySkullyEloiseIsaacLaura = 54833
 }
-let Bell = 0
-let OllieOzMarthaChachiPoppyBobbySkullyEloiseIsaacLaura = 0
-radio.onReceivedMessage(Bell, function () {
-    basic.showString("Bell Ringing.")
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    basic.pause(2000)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-})
-radio.onReceivedMessage(OllieOzMarthaChachiPoppyBobbySkullyEloiseIsaacLaura, function () {
-    basic.showString("Received, Door unlocked.")
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.pause(2000)
-    pins.digitalWritePin(DigitalPin.P0, 0)
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 48735893) {
+        basic.showString("Received, Door unlocked.")
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(2000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+    if (receivedNumber == 1) {
+        basic.showString("Bell Ringing.")
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(2000)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
 })
